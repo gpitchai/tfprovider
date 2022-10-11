2a9c9ec1a15951b59d0c6495f991c394d0312c73
Ganesh Pitchaiah <35233229+gpitchai@users.noreply.github.com>
2022-10-11 10:21:59 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@ provider "oci" {
   user_ocid = "ocid1.user.oc1..aaaaaaaaft7ewogmwla6zoah4gpapcffx4vtaqvhrurv3axosfiqjs434plq" 
   private_key_path = "$HOME/.oci/terraform.pem"
   fingerprint = "20:ef:a0:cc:50:27:46:56:f3:17:36:19:53:25:1a:61"
-  region = "us-phoenix-1"
+  region = "us-ashburn-1"
 }
